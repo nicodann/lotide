ea9dba535b79e5e31cf90a2345e860c6e4b8a069
Nico Dann <nicodann@gmail.com>
2021-10-27 14:42:53 +0000

    made findTvShowByGenre.js program

---
+++ findKeyByValue.js
@@ -0,0 +1,45 @@
+//TESTING FUNCTIONS
+
+const assertEqual = function(actual, expected) {
+
+  if (actual === expected) {
+
+    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
+
+  } else {
+    
+    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
+
+  }
+
+};
+
+//MY FUNCTION
+
+const findKeyByValue = (object, value) => {
+
+  for (let key in object) {  
+
+    if (object[key] === value) {
+
+      return key;
+
+    }  
+
+  }
+
+}
+
+//TEST ASSERTIONS
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+// findKeyByValue(bestTVShowsByGenre, "The Wire")
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
+assertEqual(findKeyByValue(bestTVShowsByGenre, "Brooklyn Nine-Nine"), 'comedy');
