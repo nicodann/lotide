db362a54aec09d862c5f77a33562fcc906129613
Nico Dann <nicodann@gmail.com>
2021-11-08 19:19:43 +0000

    seperated tests out of eqArrays

--- eqArrays.js
+++ eqArrays.js
@@ -1,17 +1,3 @@
-const assertEqual = function(actual, expected) {
-
-  if (actual === expected) {
-
-    console.log(`✅ Assertion Passed: ${actual} === ${expected}`);
-
-  } else {
-    
-    console.log(`🛑 Assertion Failed: ${actual} !== ${expected}`);
-
-  }
-
-};
-
 const eqArrays = (array1, array2) => {
 
   if (Array.isArray(array1) && Array.isArray(array2)) {
@@ -23,8 +9,6 @@ const eqArrays = (array1, array2) => {
     }
 
     for (let i = 0; i < array1.length; i++) {
-
-      // console.log(array1[i], array2[i]);
 
       if (array1[i] !== array2[i]) {
 
@@ -43,12 +27,6 @@ const eqArrays = (array1, array2) => {
 
 };
 
+module.exports = eqArrays;
 
 
-
-assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
-assertEqual(eqArrays([4, 2, 3], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], [1, 8, 3]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3, 6], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([], [1, 2, 3]), false); // => should PASS
-assertEqual(eqArrays([1, 2, 3], ['1', '2', '3']), false); // => should PASS
